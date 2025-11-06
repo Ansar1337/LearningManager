be0ce619f666d2d7e82977c6252b51a1ab4829ca
Ansar <ansarshayekin1@gmail.com>
2025-11-06 11:38:48 +0000

    feat(components): add Treeview and refactor  Articles page

---
+++ src/components/Treeview.vue
@@ -0,0 +1,68 @@
+<script setup>
+import {ref} from "vue";
+
+let props = defineProps({
+  items: Array
+})
+
+let selectedItem = defineModel('selectedItem', {default: null})
+
+// модифицируем объект, чтобы можно было управлять состояниями списков
+for (let item of props.items) {
+  if (item?.type === 'group')
+    item.hidden = ref(true);
+}
+
+function select(item) {
+  selectedItem.value = item;
+}
+</script>
+
+<template>
+  <div class="treeview">
+    <ul>
+      <template v-for="item in props.items">
+        <li v-if="item?.type === 'article'" class="child">
+          <div @click="() => select(item)">{{ item?.name }}</div>
+        </li>
+        <li v-else :class="{ 'parent-close': item.hidden, 'parent-open': !item.hidden }">
+          <div @click="item.hidden = !item.hidden">{{ item?.name }}</div>
+          <Treeview v-show="!item.hidden" v-if="item?.content?.length > 0" :items="item.content"
+                    v-model:selected-item="selectedItem"></Treeview>
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.treeview {
+  font-size: 16px;
+  line-height: 20px;
+  letter-spacing: 0;
+  padding-left: 20px;
+}
+
+.treeview * li::marker {
+  color: #828282;
+}
+
+.treeview * li {
+  cursor: pointer;
+}
+
+.parent-close {
+  list-style-type: "⯈";
+  padding-left: 5px;
+}
+
+.parent-open {
+  list-style-type: "⯆";
+  padding-left: 5px;
+}
+
+.child {
+  list-style-type: none;
+  padding-left: 5px;
+}
+</style>
--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -3,6 +3,7 @@ import Breadcrumbs from "@/components/Breadcrumbs.vue";
 import {onMounted, ref} from "vue";
 import {useCoursesStore} from "@/stores/CoursesStore.js";
 import {useRoute} from "vue-router";
+import Treeview from "@/components/Treeview.vue";
 
 onMounted(() => {
   window.scrollTo({
@@ -16,35 +17,65 @@ let coursesStore = useCoursesStore();
 let route = useRoute();
 
 let module = ref();
-let articles = ref();
-
+let articles = ref([]);
+let selectedArticle = ref();
 
 coursesStore.userCourses[route.params.id].modules[route.params.mid].then(result => module.value = result);
 coursesStore.userCourses[route.params.id].modules[route.params.mid].resources.articles.then(result => articles.value = result);
 </script>
 
 <template>
-  <div class="mb-5">
-    <div class="mt-5">
-      <Breadcrumbs></Breadcrumbs>
-    </div>
+  <div>
+    <div class="articles-container">
+      <div class="mt-5 treeview-container">
+        <Treeview v-if="articles?.length > 0" :items="articles" v-model:selected-item="selectedArticle"/>
+      </div>
+      <div class="mb-5 articles-content">
+        <div>
+          <Breadcrumbs></Breadcrumbs>
+        </div>
 
-    <div class="title">
-      <div>{{ module?.name }}</div>
-      <div class="articles-subtitle">
-        estimated completion time: ~30 minutes
+        <div class="title">
+          <div>{{ module?.name }}</div>
+          <div class="articles-subtitle">
+            estimated completion time: ~30 minutes
+          </div>
+        </div>
+
+        <div v-if="selectedArticle?.content?.status !== 'error'" v-html="selectedArticle?.content">
+        </div>
+        <div v-else>Ошибка при получении статьи</div>
       </div>
     </div>
-
-    <div>
-      Здесь будут статьи
-    </div>
-
-    <pre>{{ articles }}</pre>
   </div>
 </template>
 
 <style scoped>
+.articles-container {
+  display: flex;
+  gap: 40px;
+  flex-wrap: wrap;
+  padding-left: 40px;
+  padding-right: 40px;
+}
+
+.articles-content {
+  min-width: 375px;
+}
+
+.main-content {
+  max-width: 100%;
+  align-items: flex-start;
+}
+
+.treeview-container {
+  min-width: 150px;
+}
+
+.main-content > * {
+  width: initial;
+}
+
 .title {
   font-size: 36px;
   font-weight: 600;
